b6df0a89797edf89fe1b072b2fbd057e2e8753dd
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-02-23 20:33:44 +0700

    Garis.java

--- Praktikum 1/Garis.java
+++ Praktikum 1/Garis.java
@@ -1,1 +1,82 @@
-"// Program baru" 
+/*Nama File : Garis.java
+/*Deskripsi : berisi atribut dan method dalam class Garis
+/*Pembuat : Shafiyah
+/*Tanggal : 23 Februari 2025
+*/
+
+public class Garis {
+    Titik titikAwal;
+    Titik titikAkhir;
+    static int counterGaris = 0;
+
+    Garis() {
+        this.titikAwal = new Titik(0, 0);
+        this.titikAkhir = new Titik(1, 1);
+        counterGaris++;
+    }
+
+    Garis(Titik titikAwal, Titik titikAkhir) {
+        this.titikAwal = titikAwal;
+        this.titikAkhir = titikAkhir;
+        counterGaris++;
+    }
+
+    Titik getTitikAwal() {
+        return titikAwal;
+    }
+
+    void setTitikAwal(Titik titikAwal) {
+        this.titikAwal = titikAwal;
+    }
+
+    Titik getTitikAkhir() {
+        return titikAkhir;
+    }
+
+    void setTitikAkhir(Titik titikAkhir) {
+        this.titikAkhir = titikAkhir;
+    }
+
+    static int getCounterGaris() {
+        return counterGaris;
+    }
+
+    double getPanjang() {
+        double dx = titikAkhir.getAbsis() - titikAwal.getAbsis();
+        double dy = titikAkhir.getOrdinat() - titikAwal.getOrdinat();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    double getGradien() {
+        double dx = titikAkhir.getAbsis() - titikAwal.getAbsis();
+        double dy = titikAkhir.getOrdinat() - titikAwal.getOrdinat();
+        return dy / dx;
+    }
+
+    Titik getTitikTengah() {
+        double midX = (titikAwal.getAbsis() + titikAkhir.getAbsis()) / 2;
+        double midY = (titikAwal.getOrdinat() + titikAkhir.getOrdinat()) / 2;
+        return new Titik(midX, midY);
+    }
+
+    boolean isSejajar(Garis garisLain) {
+        return this.getGradien() == garisLain.getGradien();
+    }
+
+    boolean isTegakLurus(Garis garisLain) {
+        return this.getGradien() * garisLain.getGradien() == -1;
+    }
+
+    void printGaris() {
+        System.out.print("Titik Awal: ");
+        titikAwal.printTitik();
+        System.out.print("Titik Akhir: ");
+        titikAkhir.printTitik();
+    }
+
+    String getPersamaanGaris() {
+        double m = getGradien();
+        double c = titikAwal.getOrdinat() - m * titikAwal.getAbsis();
+        return "y = " + m + "x + " + c;
+    }
+}
