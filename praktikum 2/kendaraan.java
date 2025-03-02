dfe791c5449335d878f9d4e97458288ac4e9f89d
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-02 16:54:29 +0700

    kendaraan.java

---
+++ praktikum 2/kendaraan.java
@@ -0,0 +1,36 @@
+public class kendaraan{
+    private String noplat;
+    private String jenis;
+    
+    //konstruktor tanpa parameter
+    public kendaraan(){
+        this.noplat = " ";
+        this.jenis = " ";
+    }
+    
+    //konstruktor dengan parameter
+    public kendaraan(String noplat, String jenis){
+        this.noplat = noplat;
+        this.jenis = jenis;
+    }
+    
+    //selector(getter)
+    public String getnoplat(){
+        return noplat;
+    }
+    
+    public String getjenis(){
+        return jenis;
+    }
+    
+    //mutator (setter)
+    
+    public void setnoplat(){
+        this.noplat = noplat;
+    }
+    
+    public void setjenis(){
+        this.jenis = jenis;
+    }
+    
+}
