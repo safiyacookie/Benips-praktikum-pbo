a9f2516165a018501afbdb11782f9586613c3265
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-02 16:53:54 +0700

    Create matkul.java

---
+++ praktikum 2/matkul.java
@@ -0,0 +1,45 @@
+public class matkul{
+    private String idmatkul;
+    private String nama;
+    private int sks;
+    
+    //konstruktor tanpa parameter
+    public matkul(){
+        this.idmatkul = " ";
+        this.nama = " ";
+        this.sks = 0;
+    }
+    
+    //konstruktor dengan parameter
+    public matkul(String idmatkul, String nama, int sks){
+        this.idmatkul = idmatkul;
+        this.nama = nama;
+        this.sks = sks;
+    }
+    
+    //selector (getter)
+    public String getidmatkul(){
+        return idmatkul;
+    }
+    
+    public String getnama(){
+        return nama;
+    }
+    
+    public int getsks(){
+        return sks;
+    }
+    
+    //mutator (setter)
+    public void setidmatkul(){
+        this.idmatkul = idmatkul;
+    }
+    
+    public void setnama(){
+        this.nama = nama;
+    }
+    
+    public void setsks(){
+        this.sks = sks;
+    }
+}
