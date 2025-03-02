38db716839274ce5559355d1607f44e6717a39be
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-02 17:48:12 +0700

    main.java

--- praktikum 2/main.java
+++ praktikum 2/main.java
@@ -1,9 +1,96 @@
-public class main{
-    public static void main (String[] args){
-        Dosen D1 = new Dosen ("123", "Andi", "Informatika");
-        mahasiswa M1 = new mahasiswa("1234","citra", "informatika");
+import java.util.ArrayList;
+public class mahasiswa {
+    private String nim;
+    private String nama;
+    private String prodi;
+    private ArrayList<matkul>listmatkul;
+    private Dosen dosenwali;
+    private kendaraan kendaraan;
     
-        System.out.println("nip dosen = " + D1.getnip());
-        System.out.println("jumlah mata kuliah = " + M1.getjmlsks());
+    //konstruktor tanpa parameter
+    public mahasiswa(){
+        this.nim = " ";
+        this.nama = " ";
+        this.prodi = " ";
+        this.listmatkul = new ArrayList<>();
+        this.dosenwali = new Dosen();
+        this.kendaraan = new kendaraan();
+    }
+    
+    //konstruktor dengan parameter
+    public mahasiswa (String nim, String nama, String prodi){
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.listmatkul = new ArrayList<>(); 
+        this.dosenwali = new Dosen();
+        this.kendaraan = new kendaraan();
+    }
+    
+    //selector (getter)
+    public String getnim(){
+        return nim;
+    }
+    public String getnama(){
+        return nama;
+    }
+    public String getprodi(){
+        return prodi;
+    }
+    public Dosen getdosenwali(){
+        return dosenwali;
+    }
+    public kendaraan getkendaraan(){
+        return kendaraan;
+    }
+    
+    //mutator (setter)
+    public void setnim(String nim){
+        this.nim = nim;
+    }
+    public void setnama(String nama){
+        this.nama = nama;
+    }
+    public void setprodi(String prodi){
+        this.prodi = prodi;
+    }
+    public void setdosenwali(Dosen dosenwali){
+        this.dosenwali = dosenwali;
+    }
+    public void setkendaraan(kendaraan kendaraan){
+        this.kendaraan = kendaraan;
+    }
+    
+    public void addmatkul (matkul newmatkul){
+        listmatkul.add(newmatkul);
+    }
+    
+    public int getjmlsks (){
+        int sum = 0;
+        for (int i = 0; i < listmatkul.size(); i++){
+           sum += listmatkul.get(i).getsks(); 
+        }
+        return sum;
+    }
+    
+    public int getjumlahmatkul(){
+        return listmatkul.size();
+    }
+    
+    public void printmhs(){
+        System.out.println("NIM: " + nim);
+        System.out.println("Nama: " + nama);
+        System.out.println("Prodi: " + prodi);
+    }
+    
+    public void printdetailmhs(){
+        printmhs();
+        System.out.println("Dosen wali: " + dosenwali.getnama());
+        System.out.println("Kendaraan: " + kendaraan.getjenis());
+        System.out.println("Mata kuliah yang diambil: ");
+        for(matkul mk : listmatkul) {
+            System.out.println("- " + mk.getnama() + " (" + mk.getsks() + " SKS)");
+        }
+        System.out.println("Total SKS: " + getjmlsks());
     }
 }
