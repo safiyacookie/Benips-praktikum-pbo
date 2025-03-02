954f65bdef86bf46a506353567c2814d0bf9b76c
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-02 16:54:52 +0700

    mahasiswa.java

---
+++ praktikum 2/mahasiswa.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+public class mahasiswa {
+    private String nim;
+    private String nama;
+    private String prodi;
+    private ArrayList<matkul>listmatkul;
+    private Dosen dosenwali;
+    private kendaraan kendaraan;
+    
+    //konstruktor tanpa parameter
+    public mahasiswa(){
+        this.nim = " ";
+        this.nama = " ";
+        this.prodi = " ";
+        this.listmatkul = new ArrayList<>();
+        this.dosenwali = new Dosen();
+        this.kendaraan = new kendaraan();
+    }
+    
+    //konstruktor dengan parameter
+    public mahasiswa (String nim, String nama, String prodi){
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+    
+    //selector (getter)
+    public String getnim(){
+        return nim;
+    }
+    public String getnama(){
+        return nama;
+    }
+    public String getprodi(){
+        return prodi;
+    }
+    public Dosen getdosenwali(){
+        return dosenwali;
+    }
+    public kendaraan getkendaraan(){
+        return kendaraan;
+    }
+    
+    //mutator (setter)
+    public void setnim(String nim){
+        this.nim = nim;
+    }
+    public void setnama(String nama){
+        this.nama = nama;
+    }
+    public void setprodi(String prodi){
+        this.prodi = prodi;
+    }
+    public void setdosenwali(Dosen dosenwali){
+        this.dosenwali = dosenwali;
+    }
+    public void setkendaraan(kendaraan kendaraan){
+        this.kendaraan = kendaraan;
+    }
+    
+    public void addmatkul (matkul newmatkul){
+        listmatkul.add(newmatkul);
+    }
+    
+    public int getjmlsks (){
+        int sum = 0;
+        for (int i = 0; i < listmatkul.size(); i++){
+           sum += listmatkul.get(i).getsks(); 
+        }
+        return sum;
+    }
+    
+    
+}
