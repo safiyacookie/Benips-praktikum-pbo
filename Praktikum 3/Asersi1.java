4f35285089f2b3b4a29f6fd64e3ab7835de2b8fb
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-10 10:13:41 +0700

    Asersi1.java

--- Praktikum 3/Asersi1.java
+++ Praktikum 3/Asersi1.java
@@ -1,1 +1,13 @@
+public class Asersi {
+    public static void main(String[] args) {
+        int x = 0;
+
+        if (x > 0) {
+            System.out.println("x bilangan positif");
+        } else {
+            assert (x < 0) : "ada kesalahan kode";
+            System.out.println("x bilangan negatif");
+        }
+    }
+}
 
