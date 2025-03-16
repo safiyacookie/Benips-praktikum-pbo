04ad497f13dda3f7fb0230237b4797baa4a84479
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-16 21:25:53 +0700

    Persegi.java

---
+++ Praktikum 4/Persegi.java
@@ -0,0 +1,46 @@
+public class Persegi extends BangunDate {
+    private double sisi;
+    
+    public Persegi(){
+        setJmlSisi(4);
+    }
+    
+    public Persegi(double sisi, String warna, String border){
+        super(4, warna, border);
+        this.sisi = sisi;
+    }
+    
+    public double getSisi(){
+        return sisi;
+    }
+    
+    public void setSisi(double sisi){
+        this.sisi = sisi;
+    }
+    
+    public double getLuas(){
+        return sisi * sisi;
+    }
+    
+    public double getKeliling(){
+        return sisi * 4;
+    }
+    
+    public double getDiagonal(){
+        return sisi * Math.sqrt(2);
+    }
+    
+    @Override
+    public void printInfo(){
+        System.out.println("Warna Bangun Datar : " + getWarna());
+        System.out.println("Border Bangun Datar : "+getBorder());
+        System.out.println("Jumlah Sisi Bangun Datar : "+getJmlSisi());
+        System.out.println("sisi : " + sisi);
+    }
+    
+    @Override
+    public void printInfo(){
+        super.printInfo();
+        System.out.println("sisi : " + sisi);
+    }
+}
