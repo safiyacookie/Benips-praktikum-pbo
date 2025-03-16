32fda96d48d216004846fc98a3ba31addbe053dc
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-16 22:12:52 +0700

    Dosen.java

---
+++ Praktikum 4/Dosen.java
@@ -0,0 +1,22 @@
+/*
+File : Dosen.java
+Deskripsi : Program yang berisi class Dosen
+Pembuat : Shafiyah
+Tanggal : 16 Maret 2025
+*/
+
+public class Dosen extends Pegawai {
+    protected String Fakultas;
+
+    public Dosen(String NIP, String Nama, Date TglLahir, Date TMT, double GajiPokok, String Fakultas) {
+        super(NIP, Nama, TglLahir, TMT, GajiPokok);
+        this.Fakultas = Fakultas;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Fakultas: " + Fakultas);
+    }
+}
+
