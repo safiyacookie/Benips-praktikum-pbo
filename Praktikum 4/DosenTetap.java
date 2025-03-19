8eef0da41dc96938de9b3cd8493b6695aa123a1b
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-19 14:06:07 +0700

    Update DosenTetap.java

--- Praktikum 4/DosenTetap.java
+++ Praktikum 4/DosenTetap.java
@@ -1,9 +1,11 @@
 /*
-File : Dosen.java
-Deskripsi : Program yang berisi class Dosen
+File : DosenTetap.java
+Deskripsi : Program yang berisi class Dosen tetap
 Pembuat : Shafiyah
-Tanggal : 16 Maret 2025
+Tanggal : 19 Maret 2025
 */
+
+import java.util.Date;
 
 public class DosenTetap extends Dosen {
     private String NIDN;
