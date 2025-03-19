64b5dc461830f4d89230437e6391ed391556d914
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-03-19 14:06:48 +0700

    Update Main.java

--- Praktikum 4/Main.java
+++ Praktikum 4/Main.java
@@ -2,7 +2,7 @@
 File : Main.java
 Deskripsi : Program yang mengimplementasi class Tendik, dosentamu, dosentetap, dosen, dan pegawai
 Pembuat : Shafiyah
-Tanggal : 16 Maret 2025
+Tanggal : 19 Maret 2025
 */
 import java.util.Date;
 
@@ -34,7 +34,6 @@ public class Main {
     }
 }
 
-}
 
 
 
