52269277a4bf8862a299c8ce997886997fae62ea
shafiyah shafiyah <shafiyahshafiyah939@gmail.com>
2025-05-14 21:18:38 +0700

    Create Anabul.java

---
+++ Praktikum 7/Anabul.java
@@ -0,0 +1,10 @@
+public abstract class Anabul {
+    protected String nama;
+
+    public Anabul(String nama) {
+        this.nama = nama;
+    }
+
+    public abstract void gerak();
+    public abstract void bersuara();
+}
